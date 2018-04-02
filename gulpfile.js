23a1d05b5acb010982d9794da8ecc040a84e31f9
martinli <youjenli1124@gmail.com>
2018-04-03 02:08:55 +0800

    使 gulp 可以批次建置多個 ts 檔案

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const del = require('del');
 const tsify = require('tsify');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream'); // 將 browserify 處理好的檔案轉換回 gulp 接受的 vinyl 檔案格式
+const es = require('event-stream');
+const rename = require('gulp-rename');
 const log = require('fancy-log');
 const sourcemaps = require('gulp-sourcemaps');
 const concat = require('gulp-concat-css');
@@ -17,7 +19,7 @@ gulp.task(cleanTask, function () {
     return del(distPath);
 });
 
-const tsEntryFiles = [srcPath + '/ts/index.tsx'];
+const tsEntryFiles = [srcPath + '/ts/index.tsx', srcPath + '/ts/fixedMenu.tsx'];
 const jsArtifact = 'index.js'; //JavaScript 成品的名稱
 const tsConfig = require('./tsconfig.json');
 /*因為 tsify 接收參數的格式在 compilerOptions 的部分比 tsconfig 高一層, 
@@ -31,28 +33,35 @@ if (tsConfig.hasOwnProperty('compilerOptions')) {
 //提出 compilterOption 之後就是複製其他欄位
 Object.assign(transpileConfig, tsConfig);
 //指定要輸出 JavaScript 的檔名
-transpileConfig.outFile = jsArtifact;
+//transpileConfig.outFile = jsArtifact;
 
 function bundleJS() {
-    const result = browserify({ //browserify 會一併打包專案的依賴函式庫 , 也就是 React 和 ReactDOM
+    const tasks = tsEntryFiles.map(function(entryFile){
+        return browserify({ //browserify 會一併打包專案的依賴函式庫 , 也就是 React 和 ReactDOM
             basedir: '.',
             debug: true, //是否包含 sourcemap
-            entries: tsEntryFiles, //要打包的 js 檔起始點
+            entries: entryFile, //要打包的 js 檔起始點
             cache: {},
             packageCache: {}
         })
         .plugin(tsify, //使用 tsify 呼叫 typescript 轉譯器轉譯 typescript 原始碼
             transpileConfig
         ).bundle() //實際開始打包, 輸出成 node.js stream
-        .pipe(source(jsArtifact)) // 透過 vinyl-source-stream 轉換前面的建置成果為 gulp 可輸出的串流
+        .pipe(source(entryFile)) // 透過 vinyl-source-stream 轉換前面的建置成果為 gulp 可輸出的串流
+        .pipe(rename({
+            extname: '.bundle.js'
+        }))
         .pipe(gulp.dest(distPath));
-
-    log.info('Transpile and bundle javascript files complete.');
-    return result;
+    });
+    return es.merge.apply(null, tasks);
 }
 
 const prepareJSTask = 'prepareJS';
-gulp.task(prepareJSTask, [cleanTask], bundleJS);
+gulp.task(prepareJSTask, [cleanTask], function(){
+    const result = bundleJS();
+    log.info('Transpile and bundle javascript files complete.');
+    return result;
+});
 
 function copyCSSFiles() {
     const result = gulp.src([srcPath + '/css/**/*.css'])
